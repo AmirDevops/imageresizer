f5ec241f6c1e8b5ea4f292b1e362f15b613c8bb1
root <root@ip-172-31-37-244.us-west-2.compute.internal>
2020-12-17 13:36:56 +0000

    updated index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.handler = async (event, context, callback) => {
     const srcBucket = event.Records[0].s3.bucket.name;
     // Object key may have spaces or unicode non-ASCII characters.
     const srcKey    = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-    const dstBucket = "dev-uploaded-content";
+    const dstBucket = "dev-uploaded-content/*/*/*/ProfileImage/";
     const dstKey    = srcKey;
 
     // Infer the image type from the file suffix.
